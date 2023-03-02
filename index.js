c53d4443696be0c76d7f052685b680eb50a4f96a
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-02 21:43:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
-const http = require('http');
+
+
+
+
+
+
+
+
+
+
+/*const http = require('http');
 
 http.createServer((req,res) => {
     res.write("<h1>Hello</h1>");
     res.end();
 }).listen(4500);
+*/
 
+//App.js
+/*
+module.exports = {
+    x:10,
+    y:20,
+    z:function() {
+        return 10;
+    }
+}*/
+
+//index.js
+
+/*const app = require('./app');
+console.log("Sum");
+var a=10;
+var b=30;
+var c=40;
+console.warn(a+b+c);
+var x=20 ;
+if(x===20) {
+    console.log("matched");
+}
+for(let i=0; i<10; i++) {
+    console.log(i);
+}
+
+console.log(app.z());
+
+const arr=[2, 4, 7, 1, 3, 8, 3];
+
+let result = arr.filter((item) => {
+    return item>4;
+})
+
+console.log(result);
+
+const fsi = require('fs').writeFileSync;
+fsi("abc.txt","coding");
+
+*/
