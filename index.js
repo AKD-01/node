964cec6c9ee97c729d7a61e23d400cb32881a6b5
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-04 19:40:48 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const http = require("http");
+const data = require('./data');
+
+http.createServer((req,res) => {
+    res.writeHead(200,{'Content-Type' : 'application\json'});
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(4500);
+
+
+
+
+
+
+
+
+
+
+/*
 // Chalk Package Usage
 const chalk = require('chalk');
 console.log(chalk.red("Hello"));
-
-
-
-
-
-
-
-
+*/
 
 /*
 Colors Package usage
