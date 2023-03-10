7be26c30945a9361d5ca94ad5b961c1ea26480fd
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-10 10:54:03 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname,'public');
+
+app.use(express.static(publicPath));
+
+app.listen(5000);
+
 //Render HTML and JSON data
 /*const express = require('express');
 const app = express(); //For making express in executable state
