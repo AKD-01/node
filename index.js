d15e5944f2baa3d53d9bbf907258a97109ca6fe8
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-11 10:05:28 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname,'public');
+
+app.set('view engine', 'ejs');
+
+app.get('',(_,res) => {
+    res.sendFile(`${publicPath}/index.html`);
+});
+app.get('/about',(_,res) => {
+    res.sendFile(`${publicPath}/about.html`);
+})
+app.get('/help',(_,res) => {
+    res.sendFile(`${publicPath}/help.html`);
+})
+app.get('*',(_,res) => {
+    res.sendFile(`${publicPath}/notFound.html`);
+})
+
+app.listen(5000);
+
 //Remove extension from URL
 /*const express = require('express');
 const path = require('path');
