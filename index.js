d5282ab54bf0d60679df4cf257f7329697532da4
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-11 09:07:42 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const publicPath = path.join(__dirname,'public');
+//app.use(express.static(publicPath));
+
+app.get('',(_,res) => {
+    res.sendFile(`${publicPath}/index.html`);
+});
+app.get('/about',(_,res) => {
+    res.sendFile(`${publicPath}/about.html`);
+})
+app.get('/help',(_,res) => {
+    res.sendFile(`${publicPath}/help.html`);
+})
+app.get('*',(_,res) => {
+    res.sendFile(`${publicPath}/notFound.html`);
+})
+
+app.listen(5000);
 
 //Make HTML pages
 /*const express = require('express');
