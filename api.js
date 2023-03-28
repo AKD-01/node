afd5b359898a79d095ea0bffdff2b8cd960990fb
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-28 06:55:58 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
+
+app.get('/',(req,res) => {
+    res.send({name: 'Anjali'})
+});
+
+app.listen(5000);
