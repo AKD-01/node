e3cb2e41d46dc33401bdfe3b7749fa45100e6ffa
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-30 18:06:33 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const dbConnect = require('./mongodb');
+const express = require("express");
+const dbConnect = require("./mongodb");
 const app = express();
 
 app.use(express.json());
 
-app.get('/', async (_, res) => {
-    let data = await dbConnect();
-    data = await data.find().toArray();
-    res.send(data);
+app.get("/", async (_, res) => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  res.send(data);
 });
 
-app.post('/', async (req, res) => {
-    let data = await dbConnect();
-    let result = await data.insertOne(req.body);
-    res.send(result);
+app.post("/", async (req, res) => {
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  res.send(result);
 });
 
-app.put('/', (req, res) => {
-    res.send({result:"updated"});
+app.put("/", async (req, res) => {
+  let data = await dbConnect();
+  let result = data.updateOne({ name: req.body.name }, { $set: req.body });
+  res.send({ result: "updated" });
 });
 
 app.listen(5000);
