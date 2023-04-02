ca22a36dc2892242ea8285369c1f892909e9275c
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-04-02 21:15:26 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const os = require('os');
-console.log(os.arch());
-
+//console.log(os.arch());
+//console.log(os.freemem()/(1024*1024*1024)); //free memory in system in gb
+//console.log(os.totalmem()/(1024*1024*1024));
+//console.log(os.hostname());
+console.log(os.platform());
 
 //Upload file in node js using Multer 
 /*const express = require("express");
