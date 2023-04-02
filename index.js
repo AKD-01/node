5b67eebf67b010cc00dbf86c7987a4a576a8066d
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-04-02 09:40:18 +0530

    update index.js

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.post("/create", async (req, res) => {
   res.send(result);
 });
 
+app.get("/list", async (_, res) => {
+  let data = await Product.find();
+  res.send(data);
+});
+
+app.delete("/delete/:_id", async (req, res) => {
+  //console.log(req.params);
+  let data = await Product.deleteOne(req.params);
+  res.send(data);
+});
+
+app.put("/update/:_id", async (req, res) => {
+  //console.log(req.params);
+  let data = await Product.updateOne(req.params, { $set: req.body });
+  res.send(data);
+});
+
 app.listen(5000);
 
 //CRUD with Mongoose
