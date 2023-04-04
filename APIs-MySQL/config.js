271088ea9c11f5e572bc79cfa99398c13aee3cf2
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-04-04 14:08:22 +0530

    update config.js

--- APIs-MySQL/config.js
+++ APIs-MySQL/config.js
@@ -1,1 +1,16 @@
-const my
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+con.connect((err) => {
+  if (err) {
+    console.warn("ERROR!");
+  } else {
+    console.warn("Connected.");
+  }
+});
+
+module.exports = con;
