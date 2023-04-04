82a7e06edfa5c78856fefaa90b7462a6906f664d
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-04-04 12:53:22 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+//Node connect with MY SQL
+const mysql = require("mysql");
 
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+con.connect((err) => {
+  if (err) {
+    console.warn("error");
+  } else {
+    console.warn("connected");
+  }
+});
+
+con.query("select * from users", (err, result) => {
+  console.warn("result", result);
+});
 
 //Interview Question
 /*const x=10;
